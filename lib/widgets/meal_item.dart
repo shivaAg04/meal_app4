7ae4f26c3cbdc5a669c137ff70075b0a807cca03
Your Name <you@example.comkclear>
2021-06-15 16:08:46 +0530

    first

---
+++ lib/widgets/meal_item.dart
@@ -0,0 +1,147 @@
+import 'dart:js';
+
+import 'package:flutter/material.dart';
+import '../screens/meal_detail_screen.dart';
+import '../models/meal.dart';
+
+class MealItem extends StatelessWidget {
+  final String title;
+  final String imageUrl;
+  final int duration;
+  final Complexity complexity;
+  final Affordability affordability;
+  final String id;
+
+  MealItem(
+      {@required this.affordability,
+      @required this.complexity,
+      @required this.duration,
+      @required this.imageUrl,
+      @required this.title,
+      @required this.id});
+
+  String get complexityTest {
+    switch (complexity) {
+      case Complexity.Simple:
+        return 'simple';
+        break;
+
+      case Complexity.Challenging:
+        return 'Challenging';
+        break;
+
+      case Complexity.Hard:
+        return 'Hard';
+        break;
+      default:
+        return 'unknown';
+    }
+  }
+
+  String get affordabilityTest {
+    switch (affordability) {
+      case Affordability.Affordable:
+        return 'Affordable';
+        break;
+
+      case Affordability.Luxurious:
+        return 'Luxurious';
+        break;
+
+      case Affordability.Pricey:
+        return 'Pricey';
+        break;
+      default:
+        return 'unknown';
+    }
+  }
+
+  void selectMeal() {
+    Navigator.of(context).pushNamed(MealDetailScreen.routeName, arguments: id);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () => selectMeal(context),
+      child: Card(
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+        elevation: 4,
+        margin: EdgeInsets.all(10),
+        child: Column(
+          children: [
+            Stack(
+              children: [
+                ClipRRect(
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(15),
+                    topRight: Radius.circular(15),
+                  ),
+                  child: Image.network(
+                    imageUrl,
+                    height: 250,
+                    width: double.infinity,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                Positioned(
+                  bottom: 20,
+                  right: 13,
+                  child: Container(
+                    width: 250,
+                    color: Colors.black54,
+                    padding: EdgeInsets.all(12),
+                    child: Text(
+                      title,
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 27,
+                      ),
+                      softWrap: true,
+                      overflow: TextOverflow.fade,
+                    ),
+                  ),
+                )
+              ],
+            ),
+            Padding(
+              padding: EdgeInsets.all(20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Row(
+                    children: [
+                      Icon(Icons.schedule),
+                      SizedBox(
+                        width: 6,
+                      ),
+                      Text(' $duration  min')
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Icon(Icons.work),
+                      SizedBox(
+                        width: 6,
+                      ),
+                      Text(complexityTest),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Icon(Icons.attach_money),
+                      SizedBox(
+                        width: 6,
+                      ),
+                      Text(affordabilityTest),
+                    ],
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
