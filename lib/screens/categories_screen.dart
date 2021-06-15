4241b1c1756eb3626d1ee21af489214690102177
Your Name <you@example.comkclear>
2021-06-15 16:34:14 +0530

    second

--- lib/screens/categories_screen.dart
+++ lib/screens/categories_screen.dart
@@ -10,7 +10,7 @@ class CategoriesScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text(
-          'shivMeal',
+          'shivaMeal',
           style: TextStyle(fontWeight: FontWeight.bold),
         ),
       ),
